405db4c4ce39be12152a913cc66664e1fe62f320
ElaidKan <kanounelaid@yahoo.fr>
2024-05-01 16:33:19 +0200

    ci: suppression des élements par défaut

---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,14 @@
+function navbar() {
+  return (
+    <div>
+        <img src="kasa-logo.png" alt="logo" />
+        <ul>
+            <li>Accueil</li>
+            <li>A propos</li>
+        </ul>
+
+    </div>
+  )
+}
+
+export default navbar
